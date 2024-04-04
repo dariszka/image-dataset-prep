c829e70e334fe5be11a685476a63f43c21c3ddaf
dariszka <darinapetrova05@gmail.com>
2024-04-05 00:52:48 +0200

    First commit.

---
+++ prepare_image.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def prepare_image(image: np.ndarray,
+                    width: int,
+                    height: int,
+                    x: int,
+                    y: int,
+                    size: int
+                    ) -> tuple[np.ndarray, np.ndarray]:
+    pass
+
---
+++ to_grayscale.py
@@ -0,0 +1,5 @@
+import numpy as np
+from PIL import Image
+
+def to_grayscale(pil_image: np.ndarray) -> np.ndarray:
+    pass
